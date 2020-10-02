1ca47119cec361ef80fff6abac10c5781de9277c
Coralio Ballester <coralio.ballester@gmail.com>
2020-10-02 20:45:04 +0200

    Updates

---
+++ precache-manifest.66b520fcecdddd1bf77f0ec45e996b67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf206d4decf3cef958d6fae970e0876a",
+    "url": "/testDeploymentRobofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/testDeploymentRobofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5a3462b93026b8e68215",
+    "url": "/testDeploymentRobofriends/static/css/main.f07ae87c.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/testDeploymentRobofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testDeploymentRobofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3462b93026b8e68215",
+    "url": "/testDeploymentRobofriends/static/js/main.e8650c3f.chunk.js"
+  },
+  {
+    "revision": "7bfda650fc06511f88dd",
+    "url": "/testDeploymentRobofriends/static/js/runtime-main.f8b54a1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/testDeploymentRobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
